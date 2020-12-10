8e7d95dfb02ec19ab56e9b4b16bab47866ec94a1
DiegoMarquesETI <75643218+DiegoMarquesETI@users.noreply.github.com>
2020-12-10 22:35:05 +0100

    Update ProgrammePrincipale.py

--- ProgrammePrincipale.py
+++ ProgrammePrincipale.py
@@ -8,24 +8,3 @@ from canvasFinale import LabelCoup,LabelMotRech,BoutonEntry,BoutonProposer,Bouto
 from  tkinter import Tk,Label,Button,Entry,StringVar,Canvas,PhotoImage
 
 
-
-
-LabelCoup.grid(row=1)
-LabelMotRech.grid(row=2)
-BoutonEntry.grid(row=3)
-BoutonProposer.grid(row=4)
-BoutonRejouer.grid(row=2,column=3)
-BoutonQuitter.grid(row=3,column=3)
-LabelLettresFausses.grid(row=5)
-LabelScoreMax.grid(row=8,column=3)
-Canevas.grid(row=1,column=2,rowspan=4)
-console.grid(row=7, column=2)
-
-Rejouer()
-
-Mot=Rejouer()[0]
-LettresTrouver=Rejouer()[1]
-LettresFausses=Rejouer()[2]
-vie=Rejouer()[3]
-
-Mafenetre.mainloop()
